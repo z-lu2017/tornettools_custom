c24bba05750a72607112e20fca694a645616f7cb
Ubuntu <zhifan.lu2017@gmail.com>
2025-01-21 16:32:43 +0000

    change generate script

--- generate.sh
+++ generate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-tornettools generate relayinfo_staging_2024-05-01--2024-05-31.json userinfo_staging_2024-05-01--2024-05-31.json networkinfo_staging.gml tmodel-ccs2018.github.io --network_scale 0.005 --prefix tornet-0.005-vanilla;
+for counter in {1..10};
+do 
+	tornettools generate relayinfo_staging_2024-05-01--2024-05-31.json userinfo_staging_2024-05-01--2024-05-31.json networkinfo_staging.gml tmodel-ccs2018.github.io --network_scale 0.005 --prefix tornet-0.005-vanilla-$counter &&
+		cp -r tornet-0.005-vanilla-$counter tornet-0.005-discount-$counter &&
+		cp -r tornet-0.005-vanilla-$counter tornet-0.005-matching-$counter
+done
 
-cp -r tornet-0.005-vanilla tornet-0.005-discount;
-
-cp -r tornet-0.005-vanilla tornet-0.005-matching;
+for counter in {1..10};
+do
+	tornettools simulate tornet-0.005-vanilla-$counter
+done
